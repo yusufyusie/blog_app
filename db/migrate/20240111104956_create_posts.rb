e4883e9115552b287adcc7149da47e7ca89d61e6
yusufyusie <yesuf023@gmail.com>
2024-01-11 20:57:30 +0300

    The text column should be of type t.text so that it can save more text, as t.string has lower storage capability.

--- db/migrate/20240111104956_create_posts.rb
+++ db/migrate/20240111104956_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :text
+      t.text :title
+      t.text  :text
       t.integer :comment_counter
       t.integer :likes_counter
 
--- db/migrate/20240111105304_create_comments.rb
+++ db/migrate/20240111105304_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.string :text
+      t.text  :text
 
       t.timestamps
     end
