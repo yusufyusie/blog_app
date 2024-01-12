64dce994d826f2f6e2f30a93d9def3fe68093805
yusufyusie <yesuf023@gmail.com>
2024-01-12 15:40:38 +0300

    Save after update models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :comments
   has_many :likes
+  after_save :update_user_posts_counter
 
   def update_user_posts_counter
     user.update(posts_counter: user.posts.count)
