7f7cac0e82f5477000546150426b22c3aeabc4dc
yusufyusie <yesuf023@gmail.com>
2024-01-12 18:22:20 +0300

    Add Comment model specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   # Method specs
+   describe '#update_post_comments_counter' do
+   it 'updates the comments counter for the associated post' do
+     post = create(:post, comments_counter: 0)
+     comment = create(:comment, post: post)
+
+     comment.update_post_comments_counter
+     post.reload
+
+     expect(post.comments_counter).to eq(1)
+   end
+ end
 end
