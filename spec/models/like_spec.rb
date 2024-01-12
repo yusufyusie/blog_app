c5fa267116dea8f7d3c6bf63f638e85ff69d4dc5
yusufyusie <yesuf023@gmail.com>
2024-01-12 18:23:31 +0300

    Add like model specs

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    # Method specs
+    describe '#update_post_likes_counter' do
+    it 'updates the likes counter for the associated post' do
+      post = create(:post, likes_counter: 0)
+      like = create(:like, post: post)
+
+      like.update_post_likes_counter
+      post.reload
+
+      expect(post.likes_counter).to eq(1)
+    end
+  end
 end
