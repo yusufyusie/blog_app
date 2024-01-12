7b78997f9f6f0efb157f18dfab5e421541228ab2
yusufyusie <yesuf023@gmail.com>
2024-01-12 15:34:21 +0300

    Create custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :comments
   has_many :likes
+
+  def update_user_posts_counter
+    user.update(posts_counter: user.posts.count)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
