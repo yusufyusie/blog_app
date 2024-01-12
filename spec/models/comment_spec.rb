d5ba4914737e74d856119da24a402ea828b88752
yusufyusie <yesuf023@gmail.com>
2024-01-12 18:55:16 +0300

    Update spec files of models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-   # Method specs
-   describe '#update_post_comments_counter' do
-   it 'updates the comments counter for the associated post' do
-     post = create(:post, comments_counter: 0)
-     comment = create(:comment, post: post)
+ # Method specs
+ describe '#update_post_comments_counter' do
+ it 'updates the comments counter for the associated post' do
+   post = create(:post, comments_counter: 0)
+   comment = create(:comment, post: post)
 
-     comment.update_post_comments_counter
-     post.reload
+   comment.update_post_comments_counter
+   post.reload
 
-     expect(post.comments_counter).to eq(1)
-   end
+   expect(post.comments_counter).to eq(1)
  end
+end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
     # Method specs
-    describe '#update_post_likes_counter' do
-    it 'updates the likes counter for the associated post' do
-      post = create(:post, likes_counter: 0)
-      like = create(:like, post: post)
+  describe '#update_post_likes_counter' do
+  it 'updates the likes counter for the associated post' do
+    post = create(:post, likes_counter: 0)
+    like = create(:like, post: post)
 
-      like.update_post_likes_counter
-      post.reload
+    like.update_post_likes_counter
+    post.reload
 
-      expect(post.likes_counter).to eq(1)
-    end
+    expect(post.likes_counter).to eq(1)
   end
+end
 end
