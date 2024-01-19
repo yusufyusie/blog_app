b81a7e90378c67f62ad6a6bc1e1237160169c2b2
yusufyusie <yesuf023@gmail.com>
2024-01-19 16:38:27 +0300

    create action of post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,31 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+     if @post.save
+          redirect_to user_post_path(@user, @post), notice: 'Post created successfully!'
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+
+      # ... other actions like edit, update, destroy
+      private def set_user
+        @user = User.find(params[:user_id])
+      end
+
+      def set_post
+        @post = Post.find(params[:id])
+      end
+
+      def post_params
+        params.require(:post).permit(:title, :text)
+      end
+   end
+  end
