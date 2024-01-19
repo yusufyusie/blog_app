9e211779cab861afc530b3841db588b6728f5ad4
yusufyusie <yesuf023@gmail.com>
2024-01-19 18:07:42 +0300

    Add styles and navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
-    @posts = Post.all.paginate(page: params[:page], per_page: 2)
+    @posts = Post.all.paginate(page: params[:page], per_page: 3)
   end
 
   def show
@@ -17,8 +17,9 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
-     if @post.save
-          redirect_to user_post_path(@user, @post), notice: 'Post created successfully!'
+      if @post.save
+          flash[:success] = 'Post created successfully!'
+          redirect_to user_posts_url
         else
           render :new, status: :unprocessable_entity
         end
@@ -37,4 +38,3 @@ class PostsController < ApplicationController
         params.require(:post).permit(:title, :text)
       end
    end
-
