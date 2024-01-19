a9a10f5cad5840fcacb8be8d99d5f1e85bcb02f7
yusufyusie <yesuf023@gmail.com>
2024-01-19 12:09:50 +0300

    Update route to remove mount error

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
+
+gem "letter_opener", "~> 1.8"
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,3 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show]
   end
 end
-
-# If you specifically need the web-based console, add this line to your config/routes.rb file:
-if Rails.env.development?
-  mount LetterOpenerWeb::Engine, at: "/rails/letter_opener"
-  mount Rails::WebConsole::Engine, at: "/rails/console"
-end
