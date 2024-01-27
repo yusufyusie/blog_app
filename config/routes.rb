2de3deee6afe7a11d00bd23d1e2952e1f6bf5328
yusufyusie <yesuf023@gmail.com>
2024-01-27 06:44:10 +0300

    Update rout configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "users#index"
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'users#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'login#index', as: :unauthenticated_root
+    end
+  end
+
+  get '/login', to: 'login#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
       resources :comments, only: [:new, :create]
