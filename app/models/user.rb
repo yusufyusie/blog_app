8687557cbbe561a7373992169ddd5f83e5c7e9c5
yusufyusie <yesuf023@gmail.com>
2024-01-27 14:18:11 +0300

    Update user model to add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
 
-  # Validations
   validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def recent_posts(limit = 3)
-    posts.order(created_at: :desc).limit(limit)
+  def three_recent_posts
+    posts.order(created_at: :desc).first(3)
   end
 end
