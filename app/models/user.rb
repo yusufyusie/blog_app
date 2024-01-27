07a3cf2a34a83f5ceef66f5cc0b6a74aa61bd934
yusufyusie <yesuf023@gmail.com>
2024-01-27 16:17:01 +0300

    Fix linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
- # Validations
- validates :name, presence: true
- validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  # Validations
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
- def recent_posts(limit = 3)
-   posts.order(created_at: :desc).limit(limit)
- end
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
 end
