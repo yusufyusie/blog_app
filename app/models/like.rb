0b4df1bc679a78268c7a390b93627544ea54830a
yusufyusie <yesuf023@gmail.com>
2024-01-27 21:22:01 +0300

    Update like model to add validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,8 @@ class Like < ApplicationRecord
   belongs_to :post
   after_save :update_post_likes_counter
 
+  validates :user, uniqueness: { scope: :post }
+
   def update_post_likes_counter
     post.update(likes_counter: post.likes.count)
   end
